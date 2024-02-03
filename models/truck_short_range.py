d037cd018dd2c5c20e81019ee4482ee54aa4044d
radoooo11 <radomihaylov11@gmail.com>
2024-02-03 15:14:04 +0200

    created_subclass vehicles

---
+++ models/truck_short_range.py
@@ -0,0 +1,8 @@
+from models.vehicle import Vehicle
+
+
+class TruckShort(Vehicle):
+    def __init__(self, id: int, name: str, capacity: int, max_range: int):
+        super().__init__(id, name, capacity, max_range)
+        pass
+
