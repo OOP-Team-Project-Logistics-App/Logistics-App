b4bab11f858de573e849bf5ef1963e2d1c928c48
Rubinart <k.ivanov19981@gmail.com>
2024-02-03 15:09:14 +0200

    Temporary vehicle methods

--- models/vehicle.py
+++ models/vehicle.py
@@ -2,3 +2,20 @@ class Vehicle:
     
     def __init__(self, id: int, name: str, capacity: int, max_range: int):
         pass
+
+    def get_vehicle_id(self):
+        #get vehicle id
+        pass
+    
+    def get_name(self):
+        #get vehicle name /Scania, Man, Actros/
+        pass
+    
+    def get_capacity(self):
+        #get capacity of the truck
+        pass
+    
+    def get_max_range(self):
+        #get max_range of the truck
+        pass
+    
