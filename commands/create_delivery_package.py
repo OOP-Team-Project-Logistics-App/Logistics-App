4eb4b79c6ed5576f52d693c68ee5e85957507611
FalseBeliefs <kakvoo1@gmail.com>
2024-02-03 12:39:44 +0000

    skeleton

---
+++ commands/create_delivery_package.py
@@ -0,0 +1,16 @@
+class CreateDeliveryPackageCommand:
+    pass
+
+    def set_start_location(self):
+        pass
+
+    def set_end_location(self):
+        pass
+
+    def package_weight(self):
+        #Capacity must be between 26000kg and 42000kg
+        pass
+
+    def __str__(self):
+        "- Brisbane (Oct 10th 06:00h) → Sydney (Oct 10th 20:00h) → Melbourne (Oct 11th 18:00h) "
+        pass
