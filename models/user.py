49d799cb05c5ab48107a4753af93312b9a09bf21
Rubinart <k.ivanov19981@gmail.com>
2024-02-16 21:23:03 +0200

    Changes to user

--- models/user.py
+++ models/user.py
@@ -2,77 +2,35 @@ from models.constants.job_title import JobTitle
 
 
 class User:
-    USERNAME_LEN_MIN = 3
-    USERNAME_LEN_MAX = 16
-    USERNAME_LEN_ERR = f'Username must be between {USERNAME_LEN_MIN} and {USERNAME_LEN_MAX} characters long!'
-    USERNAME_INVALID_SYMBOLS = 'Username contains invalid symbols!'
-
-    PASSWORD_LEN_MIN = 5
-    PASSWORD_LEN_MAX = 30
-    PASSWORD_LEN_ERR = f'Password must be between {PASSWORD_LEN_MIN} and {PASSWORD_LEN_MAX} characters long!'
-    PASSWORD_INVALID_SYMBOLS = 'Password contains invalid symbols!'
-
     def __init__(self, username: str, password: str, job_title: JobTitle):
         self.username = username
         self.password = password
         self._job_title = job_title
-        self._is_employee = None
-        self._is_supervisor = None
-        self._is_manager = None
 
     @property
     def username(self):
         return self._username
 
     @username.setter
-    def username(self, name):
-        if self.USERNAME_LEN_MIN <= len(name) <= self.USERNAME_LEN_MAX:
-            if name.isalnum():
-                self._username = name
-            else:
-                raise ValueError(self.USERNAME_INVALID_SYMBOLS)
-        else:
-            raise ValueError(self.USERNAME_LEN_ERR)
+    def username(self, value):
+        if not 3 <= len(value) <= 16:
+            raise ValueError("Username must be between 3 and 16 characters long!")
+        if not value.isalnum():
+            raise ValueError("Username contains invalid symbols!")
+        self._username = value
 
     @property
     def password(self):
         return self._password
 
     @password.setter
-    def password(self, passw):
-        special_symbols = {'@', '*', '-', '_'}
-        if self.PASSWORD_LEN_MIN <= len(passw) <= self.PASSWORD_LEN_MAX:
-            if passw.isalnum():
-                self._password = passw
-            elif any(char in special_symbols for char in passw):
-                self._password = passw
-            else:
-                raise ValueError(self.PASSWORD_INVALID_SYMBOLS)
-        else:
-            raise ValueError(self.PASSWORD_LEN_ERR)
+    def password(self, value):
+        if not (6 <= len(value) <= 20):
+            raise ValueError("Password must be between 6 and 20 characters long!")
+        if not all(char.isalnum() or char in "@*-_" for char in value):
+            raise ValueError("Password contains invalid symbols!")
+        self._password = value
 
     @property
     def job_title(self):
         return self._job_title
-    
-    # @property
-    # def is_employee(self):
-    #     if self._is_employee == JobTitle.EMPLOYEE:
-    #         return True
-    #     else:
-    #         return False
-    #
-    # @property
-    # def is_supervisor(self):
-    #     if self._is_supervisor == JobTitle.SUPERVISOR:
-    #         return True
-    #     else:
-    #         return False
-    #
-    # @property
-    # def is_manager(self):
-    #     if self._is_manager == JobTitle.MANAGER:
-    #         return True
-    #     else:
-    #         return False
-
