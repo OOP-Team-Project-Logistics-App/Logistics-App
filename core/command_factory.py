8ecb9ae1928343ff9c9454c3fed3ac7da3821122
Rubinart <k.ivanov19981@gmail.com>
2024-02-18 13:34:07 +0200

    Added cmdfactory tests

--- core/command_factory.py
+++ core/command_factory.py
@@ -13,6 +13,8 @@ from commands.update_current_day import UpdateCurrentDayCommand
 from commands.package_status import PackageStatusCommand
 from commands.login import LoginCommand
 from commands.logout import LogoutCommand
+
+
 class CommandFactory:
     def __init__(self, data):
         self._app_data = data
@@ -20,18 +22,12 @@ class CommandFactory:
     def create(self, input_line):
         cmd, *params = input_line.split()
 
-        if cmd.lower() == "packagestatus":
-            return PackageStatusCommand(params, self._app_data)
-        if cmd.lower() == "updateday":
-            return UpdateCurrentDayCommand(params, self._app_data)
         if cmd.lower() == "createdeliveryroute":
             return CreateDeliveryRouteCommand(params, self._app_data)
         if cmd.lower() == "createdeliverypackage":
             return CreateDeliveryPackageCommand(params, self._app_data)
         if cmd.lower() == "searchroute":
             return SearchRouteCommand(params, self._app_data)
-        if cmd.lower() == "viewroute":
-            return ViewRouteCommand(params, self._app_data)
         if cmd.lower() == "assigntrucktoroute":
             return AssignTruckToRouteCommand(params, self._app_data)
         if cmd.lower() == "assignpackagetoroute":
@@ -40,13 +36,19 @@ class CommandFactory:
             return GetPackageInformation(params, self._app_data)
         if cmd.lower() == "viewunassignedpackages":
             return ViewUnassignedPackages(params, self._app_data)
-        if cmd.lower() == "viewallroutes":
-            return ViewInProgressRoutesCommand(params, self._app_data)
+        if cmd.lower() == "packagestatus":
+            return PackageStatusCommand(params, self._app_data)
         if cmd.lower() == "showavailabletrucks":
             return ShowAvailableTrucks(self._app_data)
-        if cmd.lower() == 'login':
+        if cmd.lower() == "viewallroutes":
+            return ViewInProgressRoutesCommand(params, self._app_data)
+        if cmd.lower() == "login":
             return LoginCommand(params, self._app_data)
-        if cmd.lower() == 'logout':
+        if cmd.lower() == "logout":
             return LogoutCommand(self._app_data)
+        if cmd.lower() == "updateday":
+            return UpdateCurrentDayCommand(params, self._app_data)
+        if cmd.lower() == "viewroute":
+            return ViewRouteCommand(params, self._app_data)
         
         raise InvalidCommand(cmd)
--- tests/test_command_factory.py
+++ tests/test_command_factory.py
@@ -1,11 +1,14 @@
 from datetime import datetime, timedelta
 import unittest
+from commands.assign_package_to_route import AssignPackageToRouteCommand
+from commands.assign_truck_to_route import AssignTruckToRouteCommand
 from commands.create_delivery_package import CreateDeliveryPackageCommand
-from commands.create_delivery_route import CreateDeliveryRouteCommand
+from commands.search_route import SearchRouteCommand
 from commands.update_current_day import UpdateCurrentDayCommand
 from core.application_data import ApplicationData
 from core.command_factory import CommandFactory
 from errors.invalid_command import InvalidCommand
+from models.route import Route
 
 
 class CommandFactory_Should(unittest.TestCase):
@@ -13,34 +16,28 @@ class CommandFactory_Should(unittest.TestCase):
     def setUp(self):
         self.app_data = ApplicationData()
         self.command_factory = CommandFactory(self.app_data)
-
-        "createdeliveryroute", 
-        "createdeliverypackage",
-        "assigntrucktoroute",
-        "assignpackagetoroute",
-        "searchroute",
+ 
         "packagestatus",
         "getpackageinfo",
         "viewunassignedpackages",
         "viewroute",
         "viewallroutes"
         
-    def test_command_createdeliveryroute_when_ValidParams(self):
+    def test_command_createdeliveryroute_whenValidParams(self):
         time = (datetime.now() + timedelta(days = 10)).strftime("%m/%d/%H")
         command = self.command_factory.create(f"createdeliveryroute {time} Brisbane Sydney")
         self.assertEqual(command.params[0], time)
         self.assertEqual(command.params[1:], ("Brisbane", "Sydney"))
         self.assertEqual(command.app_data, self.app_data)
 
-
-    def test_command_createdeliveryroute_when_SetOffTimeInPast(self):
-        time_in_past = (datetime.now() - timedelta(days=1)).strftime("%m/%d/%H")
+    def test_command_createdeliveryroute_whenSetOffTimeInPast(self):
+        time_in_past = (datetime.now() - timedelta(days = 1)).strftime("%m/%d/%H")
         command = self.command_factory.create(f"createdeliveryroute {time_in_past} Brisbane Sydney Melbourne")
         with self.assertRaises(ValueError):
             command.execute()
 
-    def test_command_createdeliveryroute_when_SetOffTimeTooFarInFuture(self):
-        time_in_future = (datetime.now() + timedelta(days=31)).strftime("%m/%d/%H")
+    def test_command_createdeliveryroute_whenSetOffTimeTooFarInFuture(self):
+        time_in_future = (datetime.now() + timedelta(days = 31)).strftime("%m/%d/%H")
         command = self.command_factory.create(f"createdeliveryroute {time_in_future} Brisbane Sydney Melbourne")
         with self.assertRaises(ValueError):
             command.execute()
@@ -52,6 +49,42 @@ class CommandFactory_Should(unittest.TestCase):
         self.assertEqual(command._params, ["Sydney", "Melbourne", "100", "test@email.com"])
         self.assertIsInstance(command, CreateDeliveryPackageCommand)
 
+    def test_command_searchroute_whenValidParams(self):
+        command = self.command_factory.create("searchroute 1")
+        self.assertIsNotNone(command)
+        self.assertEqual(command._app_data, self.app_data)
+        self.assertEqual(command._params, ["1"])
+        self.assertIsInstance(command, SearchRouteCommand)
+
+    def test_command_assigntrucktoroute_whenValidParams(self):
+        command = self.command_factory.create("assigntrucktoroute 1")
+        self.assertIsNotNone(command)
+        self.assertEqual(command._app_data, self.app_data)
+        self.assertEqual(command._params, ["1"])
+        self.assertIsInstance(command, AssignTruckToRouteCommand)
+
+    def test_command_assigntrucktoroute_whenInvalidParams(self):
+        command = self.command_factory.create("assigntrucktoroute invalidparam")
+        with self.assertRaises(ValueError):
+            command.execute()
+
+    def test_command_assignpackagetoroute_whenValidParams(self):
+        command = self.command_factory.create("assignpackagetoroute 1 1")
+        self.assertIsNotNone(command)
+        self.assertEqual(command._app_data, self.app_data)
+        self.assertEqual(command._params, ["1", "1"])
+        self.assertIsInstance(command, AssignPackageToRouteCommand)
+
+    def test_command_assignpackagetoroute_whenInvalidParams(self):
+        command = self.command_factory.create("assignpackagetoroute invalidparam")
+        with self.assertRaises(ValueError):
+            command.execute()
+
+    def test_command_searchroute_whenInvalidParams(self):
+        command = self.command_factory.create("searchroute invalidparam")
+        with self.assertRaises(ValueError):
+            command.execute()
+
     def test_command_updateday_whenValidParams(self):
         command = self.command_factory.create("updateday 1.0")
         self.assertIsNotNone(command)
@@ -59,7 +92,7 @@ class CommandFactory_Should(unittest.TestCase):
         self.assertEqual(command._params, ["1.0"])
         self.assertIsInstance(command, UpdateCurrentDayCommand)
 
-    def test_command_updateday_when_InvalidParams(self):
+    def test_command_updateday_whenInvalidParams(self):
         with self.assertRaises(ValueError):
             self.command_factory.create("updateday param1")
 
