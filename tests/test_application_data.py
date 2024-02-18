522d5c16a6aac238f03cc337d99bdee58707a4f3
Rubinart <k.ivanov19981@gmail.com>
2024-02-18 16:47:54 +0200

    Added appdata unit tests

--- tests/test_application_data.py
+++ tests/test_application_data.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 import unittest
 from core.application_data import ApplicationData
+from models.constants.job_title import JobTitle
 from models.constants.truck_status import TruckStatus
 from models.package import Package
 from models.route import Route
@@ -20,12 +21,29 @@ class ApplicationData_Should(unittest.TestCase):
         self.weight = 5000
         self.contact_info = "test@test.com"
         self.package = Package(self.start_location, self.end_location, self.weight, self.contact_info)
+        self.user = User("Employee", "Epassword", JobTitle.EMPLOYEE)
 
     def test_initializer_whenDataTypesAreCorrect(self):
         self.assertIsInstance(self.app_data.delivery_routes, tuple)
         self.assertIsInstance(self.app_data.delivery_packages, tuple)
         self.assertIsInstance(self.app_data.trucks, tuple)
         self.assertIsInstance(self.app_data.employees, tuple)
+
+    def test_add_route_whenAddsRouteProperly(self):
+        self.app_data.add_route(self.route)
+        self.assertIn(self.route, self.app_data.delivery_routes)
+
+    def test_add_package_whenAddsPackageProperly(self):
+        self.app_data.add_package(self.package)
+        self.assertIn(self.package, self.app_data.delivery_packages)
+
+    def test_initialize_trucks_whenInitializesCorrectAmount(self):
+        self.app_data.initialize_trucks()
+        self.assertEqual(len(self.app_data.trucks), 40)
+
+    def test_initialize_employees_whenInitializesCorrectAmount(self):
+        self.app_data.initialize_employees()
+        self.assertEqual(len(self.app_data.employees), 3)
 
     def test_find_suitable_truck_whenCorrectlyAssignsTruck(self):
         self.app_data.initialize_trucks()
@@ -76,3 +94,23 @@ class ApplicationData_Should(unittest.TestCase):
             self.assertIn(f"-------\n"
                         f"{truck_name}:\n"
                         f"Available: {available_trucks} trucks, Unavailable: {unavailable_trucks} trucks", truck_return_string)
+            
+    def test_logged_in_user_raisesError_whenNoUserLogged(self):
+        with self.assertRaises(ValueError):
+            self.app_data.logged_in_user
+
+    def test_has_logged_in_user_returnsUserLoggedInOrNot(self):
+        self.assertFalse(self.app_data.has_logged_in_user)
+        self.app_data._logged_user = self.user
+        self.assertTrue(self.app_data.has_logged_in_user)
+
+    def test_login_whenUserLogsSuccessfully(self):
+        self.assertIsNone(self.app_data._logged_user)
+        self.app_data.login(self.user)
+        self.assertEqual(self.app_data._logged_user, self.user)
+
+    def test_logout_whenUserLogoutsSuccessfully(self):
+        self.app_data.login(self.user)
+        self.assertEqual(self.app_data._logged_user, self.user)
+        self.app_data.logout()
+        self.assertIsNone(self.app_data._logged_user)
